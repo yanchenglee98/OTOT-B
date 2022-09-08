6fe52dd219860023e96433be7731e3492d5c1306
yanchenglee98 <yanchenglee@gmail.com>
2022-09-08 23:14:59 +0800

    be: use mongodb atlas

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,15 @@
+import * as dotenv from 'dotenv';
+
+const env = dotenv.config();
+
+if (!env) {
+    throw new Error("env file missing");
+}
+
 export default {
-    port: 3000,
+    port: parseInt(process.env.PORT) || 3000,
 
     database: {
-        url: "mongodb://localhost",
+        url: "mongodb+srv://admin:admin@contacts.l4tk4kb.mongodb.net/?retryWrites=true&w=majority",
     },
 };
