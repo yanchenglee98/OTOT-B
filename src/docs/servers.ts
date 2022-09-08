3febc037f768c562aebab03d2a1f32978f0f0218
yanchenglee98 <yanchenglee@gmail.com>
2022-09-08 23:39:01 +0800

    heroku: update deployment

--- src/docs/servers.ts
+++ src/docs/servers.ts
@@ -4,5 +4,9 @@ export default () => {
       url: 'http://localhost:3000/api', // url
       description: 'Local', // name
     },
+    {
+      url: 'https://otottaskb1.herokuapp.com/api', // url
+      description: 'Heroku', // name
+    }
   ];
 };
