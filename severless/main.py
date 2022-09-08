bf52781bd9d741d17cc9cc1f7e5a455a0b9d96cf
yanchenglee98 <yanchenglee@gmail.com>
2022-09-08 18:28:49 +0800

    update severless function

--- severless/main.py
+++ severless/main.py
@@ -8,12 +8,13 @@ WeatherUrl = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
 def severless(request):
     response = requests.get(WeatherUrl)
 
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     if response.ok:
         # clementi weather
         output = response.json()["items"][0]["forecasts"][11]
-        return json.dumps({"statusCode":200, "response":output})
+        return (output, 200, headers)
     else:
-        return json.dumps({
-            "statusCode": 500,
-            "response": "{}"
-        })
+        return ({"error": "error retrieving weather"}, 500, headers)
