709d1cf712aa5e7cb64e991ae078bab92a97ec47
yanchenglee98 <yanchenglee@gmail.com>
2022-09-08 23:51:44 +0800

    gcloud: add gcloud app engine config

--- src/docs/servers.ts
+++ src/docs/servers.ts
@@ -7,6 +7,10 @@ export default () => {
     {
       url: 'https://otottaskb1.herokuapp.com/api', // url
       description: 'Heroku', // name
+    },
+    {
+      url: 'https://otottaskb.as.r.appspot.com/api', // url
+      description: 'Google Cloud App Enginge', // name
     }
   ];
 };
